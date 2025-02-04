6bcf9db9952967dcb8757f11c63afa92279caf78
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-02-04 13:49:20 +0200

    Delete(CRUD)

--- views/posts/show.view.php
+++ views/posts/show.view.php
@@ -3,6 +3,9 @@
     require "views/components/header.php";     ?>
 <h1><?= htmlspecialchars($comments["Content"])?></h1>
 <?php $post = $_GET['ID'];?>
-<p><a href="/edit?ID=<?= $comments["ID"] ?>">Edit this post</a></p> <!-- Link to edit page
+<p><a href="/edit?ID=<?= $comments["ID"] ?>">Edit this post</a></p> <!-- Link to edit page-->
+<form action="/delete">
+<button>Ahhh</button>
+</form>
 <?php require "views/components/footer.php"; ?>
 
